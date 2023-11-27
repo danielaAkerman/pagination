7fa4123d314857759f7d6774f063b2107e27cc2a
Daniela Akerman <daniela.akerman@outlook.com>
2023-11-27 17:23:53 -0300

    pagination

--- lib/requests.ts
+++ lib/requests.ts
@@ -1,10 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 
-export function getOffsetAndLimitFromReq(req: NextApiRequest, maxLimit, MaxOffset) {
-    const queryLimit = parseInt(req.query.limit as string)
-    const limit = ((queryLimit < maxLimit) ? queryLimit : 100)
+export function getOffsetAndLimitFromReq(req: NextApiRequest, maxLimit = 100, MaxOffset = 10000) {
+    const queryLimit = parseInt(req.query.limit as string || "0")
+    const queryOffset = parseInt(req.query.offset as string || "0")
 
-    const queryOffset = parseInt(req.query.offset as string)
+    let limit = 10
+
+    if (queryLimit > 0 && queryLimit < maxLimit) {
+        limit = queryLimit
+    } else if (queryLimit > maxLimit) { limit = maxLimit }
+
     const offset = ((queryOffset < MaxOffset) ? queryOffset : 0)
     return { limit, offset }
 }
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -3,8 +3,19 @@ import { getOffsetAndLimitFromReq } from "lib/requests"
 import { productsIndex } from "lib/algolia";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    const results = await productsIndex.search(req.query.search as string)
-    res.send(
-        results
-    )
+    const { offset, limit } = getOffsetAndLimitFromReq(req)
+
+
+    const results = await productsIndex.search(req.query.search as string, {
+        hitsPerPage: limit,
+        page: offset > 1 ? Math.floor(offset / limit) : 0
+    })
+    res.send({
+        results: results.hits,
+        pagination: {
+            offset,
+            limit,
+            total: results.nbHits
+        }
+    })
 }
